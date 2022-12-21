106657433208015e907fa16ab42ee437e46f6dca
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-21 20:02:27 +0300

    Remove tree.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import Node from './node';
+import Tree from './tree';
 
 const cells = [];
 
 for (let i = 0; i < 8; i++) {
   for (let j = 0; j < 8; j++) {
-    const node = new Node([i, j]);
-    cells.push(node);
+    cells.push([i, j]);
   }
 }
 
 console.log(cells);
+
+const tree = new Tree(cells);
+console.log(tree);
+tree.prettyPrint(tree.root);
+tree.printPathBetweenNodes(tree.root, [0, 1], [5, 2]);
