a45c0513e313a65be3d10290af099b1aa3a720fd
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-21 15:16:19 +0300

    Add Tree class

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cells = [];
 
 for (let i = 0; i < 8; i++) {
   for (let j = 0; j < 8; j++) {
-    const node = new Node(i, j);
+    const node = new Node([i, j]);
     cells.push(node);
   }
 }
--- src/node.js
+++ src/node.js
@@ -1,8 +1,7 @@
 // create board cells as a node
 export default class Node {
-  constructor(x = null, y = null, left = null, right = null) {
-    this.x = x;
-    this.y = y;
+  constructor(data = [], left = null, right = null) {
+    this.data = data;
     this.left = left;
     this.right = right;
   }
