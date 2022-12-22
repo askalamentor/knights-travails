35898309e723a07fab868a5710ede99b14cdc140
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-22 14:51:21 +0300

    Remove node.js

--- src/node.js
+++
@@ -1,8 +0,0 @@
-// create board cells as a node
-export default class Cell {
-  constructor(xPos = null, yPos = null, predecessor = null) {
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.predecessor = predecessor;
-  }
-}
